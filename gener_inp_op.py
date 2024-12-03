6e797144358825ffe152cefd2db9445c69523499
xieyu135 <31264697+xieyu135@users.noreply.github.com>
2024-12-03 17:13:37 +0800

    now can generate LVCH-type op file

--- gener_inp_op.py
+++ gener_inp_op.py
@@ -2,6 +2,7 @@
 import json
 from gener_inp import generInp
 from gener_op import generOp
+from gener_op_LVC import generOp as generOpLvc
 
 def main(path='config.json'):
     config = json.load(open(path, 'r'))
@@ -12,12 +13,24 @@ def main(path='config.json'):
             nstate=config["nstate"],
             nmode=config["nmode"],
             )
-    generOp(op_type=config["op_type"],
-            n_triple=config["n_triple"],
-            op_name=config["op_name"],
-            fnm_kappa=config["fnm_kappa"],
-            fnm_omega=config["fnm_omega"],
-            fnm_vmat=config["fnm_vmat"],
-            )
+    if ('model_type' not in config) or (config['model_type']=='spin_boson'):
+        generOp(op_type=config["op_type"],
+                n_triple=config["n_triple"],
+                op_name=config["op_name"],
+                fnm_kappa=config["fnm_kappa"],
+                fnm_omega=config["fnm_omega"],
+                fnm_vmat=config["fnm_vmat"],
+                )
+    elif config['model_type']=='lvc':
+        generOpLvc(op_type=config["op_type"],
+                n_triple=config["n_triple"],
+                op_name=config["op_name"],
+                fnm_kappa=config["fnm_kappa"],
+                fnm_omega=config["fnm_omega"],
+                fnm_vmat=config["fnm_vmat"],
+                fnm_lambda=config["fnm_lambda"],
+                )
+    else:
+        print(f'The model does not exist: {config["model_type"]}')
 if __name__=='__main__':
     main(path='config.json')
